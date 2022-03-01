4536a004e34eacf233502a646193ddebcb590c15
Miguel Mariano <miguel@blogscol.com>
2022-02-28 19:02:56 -0500

    Home Page

--- public/wp-content/themes/draketech/index.php
+++ public/wp-content/themes/draketech/index.php
@@ -6,9 +6,15 @@
  */
 
 get_header();
-
-$page_id = get_option( 'page_for_posts' );
 ?>
+<div style="background: url(http://dummy-images.com/winter/dummy-2090x1290-Dugs.jpg);background-size: cover !important;" class="jumbotron bg-cover text-white">
+    <div class="container py-5 text-center">
+        <h1 class="display-4 font-weight-bold">Header</h1>
+        <p class="font-italic mb-0" style="padding: 20px 0px;">Mauris ut urna varius, molestie velit in, sodales diam. Praesent et nisi vel lacus ultrices laoreet. Sed iaculis tortor ac quam egestas.</p>
+        <a href="#" role="button" class="btn btn-primary px-5">More</a>
+    </div>
+</div>
+
 <div class="row">
 	<div class="col-md-8">
 		<?php
